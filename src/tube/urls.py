6f00063a9186fce30910aa6a00cc59e87e9bf3aa
Duke09 <debopriyo09@outlook.com>
2020-08-18 12:46:29 +0530

    registration functions and templates

--- src/tube/urls.py
+++ src/tube/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
+
+from .views import Registration
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', Registration.as_view(), name='register'),
     path('', include('vid.urls', namespace='vid')),
 ]
